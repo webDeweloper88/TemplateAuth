1fde071edbee0c060d9894a27370ee6c906654dd
Makhmud <test@tets.com>
2024-09-03 17:49:15 +0500

    feat: добавление настройки конфигурации с помощью @nestjs/config
    - Интеграция @nestjs/config для управления конфигурацией
    - Добавлены конфигурации в функцию инициализации приложения, включая динамическое получение порта
    - Обновлена функция `bootstrap` для использования порта из сервиса конфигурации
    	new file:   src/config/configurations.ts
    	modified:   src/main.ts
    	modified:   src/modules/app/app.module.ts

---
+++ src/config/configurations.ts
@@ -0,0 +1,27 @@
+// src/config/configurations.ts
+import { registerAs } from '@nestjs/config';
+
+export default registerAs('config', () => ({
+  database: {
+    host: process.env.DATABASE_HOST,
+    port: +process.env.DATABASE_PORT || 5432,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    name: process.env.DATABASE_NAME,
+  },
+  jwt: {
+    secret: process.env.JWT_SECRET,
+    expiresIn: process.env.JWT_EXPIRY || '2h',
+  },
+  mail: {
+    host: process.env.MAIL_HOST,
+    port: +process.env.MAIL_PORT || 465,
+    user: process.env.MAIL_USER,
+    password: process.env.MAIL_PASSWORD,
+    from: process.env.MAIL_FROM,
+  },
+  app: {
+    url: process.env.APPLICATION_URL,
+    frontendUrl: process.env.FRONTEND_URL,
+  },
+}));
