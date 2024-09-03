5c8eac638717840ea8202059dd16f5fd02cfbd8a
Makhmud <test@tets.com>
2024-09-03 17:56:52 +0500

    feat: добавление и настройка ValidationPipe
    - Установлены пакеты class-validator и class-transformer
    - Настроен глобальный ValidationPipe для обработки и валидации входящих данных
    - Добавлены примеры DTO с правилами валидации
    - Обновлены контроллеры для использования DTO с валидацией
    
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { AppModule } from '@modules/app/app.module';
+import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+  // Настройка глобального ValidationPipe
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true, // Преобразование входных данных в соответствующие типы
+      whitelist: true, // Удаление неописанных свойств
+      forbidNonWhitelisted: true, // Генерация ошибок для неописанных свойств
+    }),
+  );
+
   const port = configService.get<number>('port');
   await app.listen(port);
 }
