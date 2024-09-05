abd58c404fcedd7d45d6d92232e562cae2fbe497
Your Name <you@example.com>
2024-09-05 23:28:06 +0500

    modified:   src/config/configurations.ts

--- src/config/configurations.ts
+++ src/config/configurations.ts
@@ -5,9 +5,9 @@ export default registerAs('config', () => ({
   database: {
     host: process.env.DATABASE_HOST,
     port: +process.env.DATABASE_PORT || 5432,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    name: process.env.DATABASE_NAME,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    name: process.env.POSTGRES_DB,
   },
   jwt: {
     secret: process.env.JWT_SECRET,
